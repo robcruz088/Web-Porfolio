1705fb3b124db647ed05773dd25cfef482f50417
Roberto Cruz <robcruz088@gmail.com>
2020-05-21 15:33:56 -0400

    Update homepage.py
    update

--- homepage.py
+++ homepage.py
@@ -1,10 +1,11 @@
 from flask import Flask, redirect, url_for, render_template
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return "Deployed to Heroku"
+    return "Simple Test"
 
 if __name__ == '__main__':
     app.run()
