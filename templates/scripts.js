eef1d64e9c46ac72360e81c1d1ce3b3eb852c69b
Roberto Cruz <robcruz088@gmail.com>
2020-05-24 22:57:51 -0400

    changes
    changes to homepages and scripts

--- templates/scripts.js
+++ templates/scripts.js
@@ -1,2 +1,22 @@
-(function ($) {
-    "use strict"; // Start of use strict
+// execute the function when the user scrolls the page
+
+window.onscroll = function() {scrollFunction()};
+
+// Get navbar
+var navbar = document.getElementById("navbar");
+
+// Get offset position of the navbar
+
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when scroll position is reached
+// Remove sticky when scroll position is left
+
+function scrollFunction(){
+    if (window.pageYOffset >= sticky){
+        navbar.classList.add("sticky")
+    }
+    else{
+        navbar.classList.remove("sticky");
+    }
+}
