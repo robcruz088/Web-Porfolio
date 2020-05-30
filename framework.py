2c832955c9994c930a28bd620c4ae47df5684f2e
Roberto Cruz <robcruz088@gmail.com>
2020-05-30 18:09:19 -0400

    Added files + changes to backend
    refactored homepage.py to framework.py for a more logical naming convention
    
    added the html for the various different pages
    
    some changes to the homepage html so that it is able to function with the framework

---
+++ framework.py
@@ -0,0 +1,33 @@
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__, static_url_path='', static_folder='templates/static', template_folder='templates')
+
+@app.route('/')
+def homePage():
+    return render_template("homepage.html")
+
+@app.route('/portfolios/')
+def portfolios():
+    return render_template("Portfolios.html")
+
+### START: THE VARIOUS PORTFOLIO PAGES ###
+
+@app.route('/roberto/')
+def robertoPortfolio():
+    return render_template("robertoPortfolio.html")
+
+### END: THE VARIOUS PORTFOLIO PAGES ###
+
+@app.route('/about/')
+def about():
+    return render_template('about.html')
+
+@app.route('/contactus/')
+def contactUs():
+    return render_template('contactus.html')
+
+
+if __name__ == '__main__':
+    # for when it needs to be deployed on AWS webservice
+    # app.run(host='0.0.0.0', port='80')
+    app.run()
